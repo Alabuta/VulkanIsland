991cd0a53548426a696402b307f362ec20b3e452
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-02-27 23:29:47 +0300

    Queues in progress.

--- VulkanIsland/src/queues.h
+++ VulkanIsland/src/queues.h
@@ -10,27 +10,9 @@ public:
     VkQueue &handle() const { return handle_; }
 
     template<class... Args>
-    [[nodiscard]] constexpr bool StrictMatching(Args &&... args)
+    [[nodiscard]] static bool constexpr supported_by_device(Args &&... args)
     {
-        return static_cast<T *>(this)->StrictMatching(std::forward<Args>(args)...);
-    }
-
-    template<class... Args>
-    [[nodiscard]] constexpr bool TolerantMatching(Args &&... args)
-    {
-        return static_cast<T *>(this)->TolerantMatching(std::forward<Args>(args)...);
-    }
-
-    /*template<class... Args>
-    VulkanQueue(Args &&... args)
-    {
-    *static_cast<T *>(this) = std::move(T(std::forward<Args>(args)...));
-    }*/
-
-    template<class... Args>
-    [[nodiscard]] static bool constexpr has(Args &&... args)
-    {
-        return T::has(std::forward<Args>(args)...);
+        return T::supported_by_device(std::forward<Args>(args)...);
     }
 
 private:
@@ -41,24 +23,6 @@ private:
 
 class GraphicsQueue final : VulkanQueue<GraphicsQueue> {
 public:
-
-    template<class P, std::enable_if_t<std::is_same_v<std::decay_t<P>, VkQueueFamilyProperties>>...>
-    [[nodiscard]] constexpr bool StrictMatching(P &&properties)
-    {
-        return properties.queueCount > 0 && properties.queueFlags == (VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT);
-    }
-
-    template<class P, std::enable_if_t<std::is_same_v<std::decay_t<P>, VkQueueFamilyProperties>>...>
-    [[nodiscard]] constexpr bool TolerantMatching(P &&properties)
-    {
-        return properties.queueCount > 0 && (properties.queueFlags & (VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT)) == (VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT);
-    }
-
-    /*template<class T, class...>
-    GraphicsQueue(T &&queueFamilies)
-    {
-    static_assert(std::is_same_v<typename std::decay_t<T>::value_type, VkQueueFamilyProperties>, "iterable object does not contain VkQueueFamilyProperties elements");
-    }*/
 
     template<class...>
     [[nodiscard]] static std::optional<std::uint32_t> supported_by_device(VkPhysicalDevice physicalDevice)
@@ -96,30 +60,8 @@ public:
 
 class TransferQueue final : VulkanQueue<TransferQueue> {
 public:
-
-    template<class P, std::enable_if_t<std::is_same_v<std::decay_t<P>, VkQueueFamilyProperties>>...>
-    [[nodiscard]] constexpr bool StrictMatching(P &&properties)
-    {
-        return properties.queueCount > 0 && properties.queueFlags == VK_QUEUE_TRANSFER_BIT;
-    }
-
-    template<class P, std::enable_if_t<std::is_same_v<std::decay_t<P>, VkQueueFamilyProperties>>...>
-    [[nodiscard]] constexpr bool TolerantMatching(P &&properties)
-    {
-        return properties.queueCount > 0 && (properties.queueFlags & VK_QUEUE_TRANSFER_BIT) == VK_QUEUE_TRANSFER_BIT;
-    }
 };
 
 class PresentationQueue final : VulkanQueue<PresentationQueue> {
 public:
-
-    template<class...>
-    [[nodiscard]] constexpr bool StrictMatching(/*VulkanDevice const &device,*/VkPhysicalDevice physicalDevice, VkSurfaceKHR surface, std::uint32_t queueIndex)
-    {
-        VkBool32 surfaceSupported = 0;
-        if (auto result = vkGetPhysicalDeviceSurfaceSupportKHR(/*device.physical_handle()*/physicalDevice, queueIndex, surface, &surfaceSupported); result != VK_SUCCESS)
-            throw std::runtime_error("failed to retrieve surface support: "s + std::to_string(result));
-
-        return surfaceSupported == VK_TRUE;
-    }
 };
