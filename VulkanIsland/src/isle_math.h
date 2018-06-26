9041a2c076c0644867effda5e84f35efaae8fbd2
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-06-26 22:36:22 +0300

    Minor changes.

--- VulkanIsland/src/isle_math.h
+++ VulkanIsland/src/isle_math.h
@@ -111,7 +111,7 @@ struct mat4 {
         std::uninitialized_copy_n(std::begin(tr.xyz), std::size(tr.xyz), std::begin(m) + 4 * 3);
     }
 
-    template<class... Ts, typename std::enable_if_t<std::conjunction_v<std::is_arithmetic<Ts>...> && sizeof...(Ts) == 16>...>
+    template<class... Ts, typename std::enable_if_t<std::conjunction_v<std::is_arithmetic<Ts>...> && sizeof...(Ts) == 16, int> = 0>
     constexpr mat4(Ts... values) : m({{ static_cast<std::decay_t<decltype(m)>::value_type>(values)... }}) { }
 };
 
