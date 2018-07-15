da67e4e5b871352cc9925091f83ed1bc71dce302
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-07-15 16:59:44 +0300

    Removed ghost files and directories. CRT Debug compilation branches.

--- VulkanIsland/src/main.cxx
+++ VulkanIsland/src/main.cxx
@@ -5,8 +5,7 @@
 #pragma comment(lib, "vulkan-1.lib")
 #pragma comment(lib, "glfw3.lib")
 #endif
-
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && defined(DEBUG)
 #define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
 #endif
@@ -1516,7 +1515,7 @@ struct SceneGraph final {
 
 int main()
 try {
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && defined(DEBUG)
     _CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_ALLOC_MEM_DF | _CRTDBG_DELAY_FREE_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     //_CrtSetBreakAlloc(84);
 #endif
