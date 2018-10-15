1b1d4f164daf3a80b868aa6c988b4b06352786ca
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-10-15 18:01:22 +0300

    [WIP] Descriptors.

---
+++ VulkanIsland/src/descriptor.cxx
@@ -0,0 +1,28 @@
+#include "descriptor.hxx"
+
+std::shared_ptr<VulkanDescriptorPool>
+DescriptorsManager::CreateDescriptorPool() noexcept
+{
+    std::shared_ptr<VulkanDescriptorPool> descriptorPool;
+
+    std::array<VkDescriptorPoolSize, 2> constexpr poolSizes{{
+        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1 },
+        { VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1 }
+    }};
+
+    VkDescriptorPoolCreateInfo const createInfo{
+        VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
+        nullptr, 0,
+        1,
+        static_cast<std::uint32_t>(std::size(poolSizes)), std::data(poolSizes)
+    };
+
+    VkDescriptorPool handle;
+
+    if (auto result = vkCreateDescriptorPool(device_.handle(), &createInfo, nullptr, &handle); result != VK_SUCCESS)
+        std::cerr << "failed to create descriptor pool: "s  << result << '\n';
+
+    else descriptorPool = std::make_shared(new VulkanDescriptorPool{handle});
+
+    return descriptorPool;
+}
---
+++ VulkanIsland/src/descriptor.hxx
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <optional>
+#include <memory>
+
+#include "main.hxx"
+#include "device.hxx"
+
+class DescriptorsManager;
+
+class VulkanDescriptorPool final {
+public:
+
+    VkDescriptorPool handle() const noexcept { return handle_; }
+
+    VulkanDescriptorPool(VkDescriptorPool handle) noexcept : handle_{handle} { }
+
+private:
+    VkDescriptorPool handle_;
+
+    VulkanDescriptorPool() = delete;
+    VulkanDescriptorPool(VulkanDescriptorPool const &) = delete;
+    VulkanDescriptorPool(VulkanDescriptorPool &&) = delete;
+
+    friend DescriptorsManager;
+};
+
+class DescriptorsManager final {
+public:
+
+
+    DescriptorsManager(VulkanDevice &device) noexcept : device_{device} { }
+
+    [[nodiscard]] std::shared_ptr<VulkanDescriptorPool>
+    CreateDescriptorPool() noexcept;
+
+private:
+
+    VulkanDevice &device_;
+};
+
+#if 0
+class VulkanDescriptorSet final {
+public:
+
+    VulkanDescriptorSet() = default;
+//    VulkanDescriptorSet(VkImageView handle, VkImageViewType type) noexcept : handle_{handle}, type_{type} { }
+
+    VkDescriptorSet handle() const noexcept { return handle_; }
+
+private:
+    VkDescriptorSet handle_;
+};
+#endif
