c29f1e90ef3c0ce5d0cd9d8c4e8d1a0f9290ef03
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-12-10 17:32:48 +0200

    [WIP] Window systems.

---
+++ VulkanIsland/src/platform/window.cxx
@@ -0,0 +1,73 @@
+#include <stdexcept>
+
+#include "window.hxx"
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+Window::Window(std::string_view name, std::int32_t width, std::int32_t height)
+    : handle_{nullptr}, width_{width}, height_{height}, name_{name}
+{
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    handle_ = glfwCreateWindow(width_, height_, name_.c_str(), nullptr, nullptr);
+
+    if (handle_ == nullptr)
+        throw std::runtime_error("failed to create '"s + name_ + "' window"s);
+
+    glfwSetWindowUserPointer(handle_, this);
+
+    glfwSetWindowSizeCallback(handle_, [] (auto handle, auto width, auto height)
+    {
+        auto instance = reinterpret_cast<Window *>(glfwGetWindowUserPointer(handle));
+
+        if (instance) {
+            instance->width_ = width;
+            instance->height_ = height;
+
+            instance->resizeCallback_(width, height);
+        }
+    });
+
+    glfwSetCursorPosCallback(handle_, [] (auto handle, auto x, auto y)
+    {
+        auto instance = reinterpret_cast<Window *>(glfwGetWindowUserPointer(handle));
+
+        if (instance) {
+            InputManager::MouseInputData mouse;
+
+            mouse.x = static_cast<decltype(mouse.x)>(x);
+            mouse.y = static_cast<decltype(mouse.y)>(y);
+
+            InputManager::InputData data = std::move(mouse);
+
+            instance->inputUpdateCallback_(data);
+        }
+    });
+
+    // glfwSetMouseButtonCallback(handle_, [] (auto handle, auto button, auto action, auto)
+    // {
+    //     auto instance = reinterpret_cast<Window *>(glfwGetWindowUserPointer(handle));
+
+    //     if (instance) {
+    //         InputManager::MouseInputData data;
+
+    //         data.x = static_cast<decltype(data.x)>(x);
+    //         data.y = static_cast<decltype(data.y)>(y);
+
+    //         instance->inputProcessCallback(std::move(data));
+    //     }
+    // });
+}
+
+Window::~Window()
+{
+    if (handle_)
+        glfwDestroyWindow(handle_);
+}
+
+void Window::connectEventHandler(std::shared_ptr<IEventHandler> handler)
+{
+    resizeCallback_.connect(decltype(resizeCallback_)::slot_type(&IEventHandler::onResize, handler.get(), _1, _2).track_foreign(handler));
+    inputUpdateCallback_.connect(decltype(inputUpdateCallback_)::slot_type(&IEventHandler::onInputUpdate, handler.get(), _1).track_foreign(handler));
+}
---
+++ VulkanIsland/src/platform/window.hxx
@@ -0,0 +1,40 @@
+#pragma once 
+
+#include <cstdint>
+#include <string>
+#include <string_view>
+
+#include <boost/signals2.hpp>
+
+#include <GLFW/glfw3.h>
+
+#include "../input/input_manager.hxx"
+
+
+class Window final {
+public:
+
+    Window(std::string_view name, std::int32_t width, std::int32_t height);
+    ~Window();
+
+    class IEventHandler {
+    public:
+
+        virtual ~IEventHandler() = default;
+
+        virtual void onResize(std::int32_t width, std::int32_t height) = 0;
+        virtual void onInputUpdate(InputManager::InputData &data) = 0;
+    };
+
+    void connectEventHandler(std::shared_ptr<IEventHandler> handler);
+
+private:
+    GLFWwindow *handle_;
+
+    std::int32_t width_{0}, height_{0};
+
+    std::string name_;
+
+    boost::signals2::signal<void(std::int32_t, std::int32_t)> resizeCallback_;
+    boost::signals2::signal<void(InputManager::InputData &)> inputUpdateCallback_;
+};
