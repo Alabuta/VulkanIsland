4bd9a8771b969621b5366ee27d7bff85e09ddf3e
Almaz Kinziabulatov <auqolaq@gmail.com>
2019-03-05 20:30:50 +0200

    Minor changes.

--- VulkanIsland/src/resources/resource.hxx
+++ VulkanIsland/src/resources/resource.hxx
@@ -15,57 +15,9 @@ class VulkanSampler;
 class VulkanBuffer;
 class VulkanShaderModule;
 
+class VertexBuffer;
+class IndexBuffer;
 
-class IndexBuffer final {
-public:
-
-    template<class T, typename std::enable_if_t<is_one_of_v<T, std::uint16_t, std::uint32_t>>...>
-    IndexBuffer(std::shared_ptr<VulkanBuffer> buffer, std::size_t size) noexcept : buffer{buffer}, size{size}, type{T{}} { }
-
-    template<class T, typename std::enable_if_t<std::is_same_v<IndexBuffer, std::decay_t<T>>>...>
-    bool constexpr operator< (T &&rhs) const noexcept
-    {
-        return buffer->handle() < rhs.buffer->handle();
-    }
-
-private:
-    std::shared_ptr<VulkanBuffer> buffer{nullptr};
-    std::size_t size{0};
-
-    std::variant<std::uint16_t, std::uint32_t> type;
-};
-
-class VertexBuffer final {
-public:
-
-    VertexBuffer(std::shared_ptr<VulkanBuffer> deviceBuffer, std::shared_ptr<VulkanBuffer> stagingBuffer, std::size_t sizeInBytes, std::uint32_t vertexInputBinding) noexcept
-        : deviceBuffer_{deviceBuffer}, stagingBuffer_{stagingBuffer}, sizeInBytes_{sizeInBytes}, vertexInputBinding_{vertexInputBinding} { }
-
-    VulkanBuffer const &deviceBuffer() const noexcept { return *deviceBuffer_; }
-
-    template<class T, typename std::enable_if_t<std::is_same_v<VertexBuffer, std::decay_t<T>>>...>
-    bool constexpr operator< (T &&rhs) const noexcept
-    {
-        return vertexInputBinding_ < rhs.vertexInputBinding_;
-    }
-
-private:
-
-    std::shared_ptr<VulkanBuffer> deviceBuffer_{nullptr};
-    std::shared_ptr<VulkanBuffer> stagingBuffer_{nullptr};
-
-    std::size_t sizeInBytes_{0};
-    std::size_t offset_{0};
-
-    std::uint32_t vertexInputBinding_;
-};
-
-struct StagingVertexBuffer final {
-    std::shared_ptr<VertexBuffer> buffer;
-
-    std::size_t sizeInBytes{0};
-    std::byte *begin;
-};
 
 
 class ResourceManager final {
