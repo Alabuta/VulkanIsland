b026d7ecc8b4ac8545941a75aee372562fb67532
Almaz Kinziabulatov <auqolaq@gmail.com>
2019-07-14 23:10:27 +0300

    Shaders compilation in progress.

--- compile-materials.py
+++ compile-materials.py
@@ -56,30 +56,54 @@ def preprocess_vertex_shader(material_data, shader_module):
     module_name = shader_module['name']
     shader_module_absolute_path = os.path.join(shaders.source_path, module_name + '.glsl')
 
-    with open(shader_module_absolute_path, 'rb') as file:
-        source_code = file.read().decode("utf-8")
+    version_string = '#version {}\n'.format(shaders.glslSettings.version)
 
-        version_string = '#version {}\n'.format(shaders.glslSettings.version)
+    extensions_string = ''
 
-        extensions_string = ''
+    for extension, behavior in shaders.glslSettings.extensions.items():
+        extensions_string += '#extension {} : {}\n'.format(extension, behavior)
 
-        for extension, behavior in shaders.glslSettings.extensions.items():
-            extensions_string += '#extension {} : {}\n'.format(extension, behavior)
+    extensions_string += '\n'
 
-        extensions_string += '\n'
+    vertex_attributes_string = ''
 
-        vertex_attributes_string = ''
+    for vertex_attribute in material_data['vertexAttributes']:
+        semantic, type = [vertex_attribute[key] for key in ('semantic', 'type')]
 
-        for vertex_attribute in material_data['vertexAttributes']:
-            semantic, type = [vertex_attribute[key] for key in ('semantic', 'type')]
+        layout = shaders.vertex_attributes_layout[semantic]
 
-            layout = shaders.vertex_attributes_layout[semantic]
+        vertex_attributes_string += 'layout (location = {}) in {} {};\n'.format(layout, type, semantic)
 
-            vertex_attributes_string += 'layout (location = {}) in {} {}\n'.format(layout, type, semantic)
+    shader_header = '{}\n{}\n{}\n#line 0'.format(version_string, extensions_string, vertex_attributes_string)
 
-        shader_header = '{}\n{}\n{}\n#line -1'.format(version_string, extensions_string, vertex_attributes_string)
+    source_code = ''
 
-        print(shader_header)
+    with open(shader_module_absolute_path, 'r') as file:
+        #source_code = file.read().decode("utf-8")
+
+        for line in file:
+            line = re.sub(
+                r'[ |\t]*#[ |\t]*pragma[ |\t]+technique[ |\t]*\((\d+)\)',
+                lambda match_object : 'void technique{}()'.format(match_object.group(1)),
+                #lambda match_object : 'void technique_{}()'.format(chr(ord('a') + int(match_object.group(1)))),
+                line
+            )
+
+            source_code += line
+
+    for technique in re.findall(r'void (technique\d)\(\)', source_code):
+        full_source_code = '{}\n{}'.format(shader_header, source_code)
+
+        source_path = os.path.join(shaders.source_path, module_name)
+
+        with open(source_path, 'w+') as file:
+            file.write(full_source_code)
+
+        entry_point = technique
+        output_path = os.path.join(shaders.source_path, '{}.{}.spv'.format(module_name, entry_point))
+        print(entry_point, output_path)
+
+        call([shaders.compiler_path, '-V', '-I' + shaders.include_path, '-e', entry_point, '-o', output_path, source_path])
 
 #def compile_shader(path):
 #    ;
