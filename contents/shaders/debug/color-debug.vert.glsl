11ed28918bcdf8929a97c8b986e86c26b5c7e581
Almaz Kinziabulatov <auqolaq@gmail.com>
2019-07-15 20:07:42 +0300

    Added missing shader source files.

---
+++ contents/shaders/debug/color-debug.vert.glsl
@@ -0,0 +1,42 @@
+
+layout (set = 0, binding = 0, scalar) readonly buffer PER_CAMERA
+{
+    mat4 view;
+    mat4 projection;
+
+    mat4 projectionView;
+
+    mat4 invertedView;
+    mat4 invertedProjection;
+} camera;
+
+layout (set = 0, binding = 2, scalar) readonly buffer PER_OBJECT
+{
+    mat4 world;
+    mat4 normal;
+} object;
+
+//layout (constant_id = 0) const int technique = 0;
+
+layout (location = 0) out vec4 outColor;
+
+out gl_PerVertex {
+    vec4 gl_Position;
+};
+
+
+#pragma technique(0)
+{
+    gl_Position = camera.view * object.world * vec4(POSITION, 1.0);
+    gl_Position = camera.projection * gl_Position;
+
+    outColor = COLOR_0;
+}
+
+#pragma technique(1)
+{
+    gl_Position = camera.view * object.world * vec4(POSITION, 1.0);
+    gl_Position = camera.projection * gl_Position;
+
+    outColor = COLOR_0;
+}
---
+++ contents/shaders/debug/normal-debug.vert.glsl
@@ -0,0 +1,39 @@
+#version 460
+
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_EXT_scalar_block_layout : enable
+
+
+layout (set = 0, binding = 0, scalar) readonly buffer PER_CAMERA
+{
+    mat4 view;
+    mat4 projection;
+
+    mat4 projectionView;
+
+    mat4 invertedView;
+    mat4 invertedProjection;
+} camera;
+
+layout (set = 0, binding = 2, scalar) readonly buffer PER_OBJECT
+{
+    mat4 world;
+    mat4 normal;
+} object;
+
+layout(constant_id = 0) const int technique = 0;
+
+layout (location = 0) out vec4 outColor;
+
+out gl_PerVertex {
+    vec4 gl_Position;
+};
+
+
+#pragma technique(0)
+{
+    gl_Position = camera.view * object.world * vec4(POSITION, 1.0);
+    gl_Position = camera.projection * gl_Position;
+
+    outColor = vec4(normalize(vec3(object.normal * vec4(NORMAL, 0.0))), 1.0);
+}
