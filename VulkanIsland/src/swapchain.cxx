2c2e36291eeac5da23ff47b7b18d877dade91587
Almaz Kinziabulatov <auqolaq@gmail.com>
2019-09-22 12:44:43 +0300

    Minor fix related to swapchain framebuffers' attachment image usage debug layer warning.

--- VulkanIsland/src/swapchain.cxx
+++ VulkanIsland/src/swapchain.cxx
@@ -124,7 +124,7 @@ CreateColorAttachement(VulkanDevice &device, TransferQueue transferQueue, VkComm
 
     auto constexpr mipLevels = 1u;
 
-    auto constexpr usageFlags = graphics::IMAGE_USAGE::TRANSIENT_ATTACHMENT | graphics::IMAGE_USAGE::COLOR_ATTACHMENT | graphics::IMAGE_USAGE::TRANSFER_DESTINATION;
+    auto constexpr usageFlags = graphics::IMAGE_USAGE::TRANSIENT_ATTACHMENT | graphics::IMAGE_USAGE::COLOR_ATTACHMENT/* | graphics::IMAGE_USAGE::TRANSFER_DESTINATION*/;
     auto constexpr propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
 
     auto constexpr tiling = graphics::IMAGE_TILING::OPTIMAL;
