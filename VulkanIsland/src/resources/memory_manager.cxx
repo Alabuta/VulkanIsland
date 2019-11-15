5e28e482291f10421936b987bc086f5c2ea09b75
Almaz Kinziabulatov <auqolaq@gmail.com>
2019-11-15 07:45:11 +0200

    Memory manager refactoring in progress.

--- VulkanIsland/src/resources/memory_manager.cxx
+++ VulkanIsland/src/resources/memory_manager.cxx
@@ -1,1 +1,82 @@
+#include <unordered_map>
+#include <vector>
+
+#include <fmt/format.h>
+
+#include "graphics/graphics_api.hxx"
+
+#include "buffer.hxx"
+#include "image.hxx"
+
 #include "memory_manager.hxx"
+
+
+namespace
+{
+    std::optional<std::uint32_t>
+    find_memory_type_index(vulkan::device const &device, std::uint32_t filter, graphics::MEMORY_PROPERTY_TYPE memory_property_types) noexcept
+    {
+        VkPhysicalDeviceMemoryProperties memory_properties;
+        vkGetPhysicalDeviceMemoryProperties(device.physical_handle(), &memory_properties);
+
+        auto const memory_types = mpl::to_array(memory_properties.memoryTypes);
+
+        auto it_type = std::find_if(std::cbegin(memory_types), std::cend(memory_types), [filter, memory_property_types, i = 0u] (auto type) mutable
+        {
+            auto const property_flags = convert_to::vulkan(memory_property_types);
+
+            return (filter & (1u << i++)) && (type.propertyFlags & property_flags) == property_flags;
+        });
+
+        if (it_type < std::next(std::cbegin(memory_types), memory_properties.memoryTypeCount))
+            return static_cast<std::uint32_t>(std::distance(std::cbegin(memory_types), it_type));
+
+        return { };
+    }
+}
+
+namespace resource
+{
+    resource::memory_manager::memory_manager(vulkan::device const &device) : device_{device}
+    {
+        buffer_image_granularity_ = device.device_limits().buffer_image_granularity;
+
+        if (kBLOCK_ALLOCATION_SIZE < buffer_image_granularity_)
+            throw std::runtime_error("default memory page size is less than buffer image granularity size"s);
+    }
+
+    resource::memory_manager::~memory_manager()
+    {
+        ;
+    }
+
+    template<>
+    std::shared_ptr<resource::device_memory>
+    memory_manager::allocate_memory(std::shared_ptr<resource::buffer> buffer, graphics::MEMORY_PROPERTY_TYPE memory_property_types)
+    {
+        auto constexpr linear_memory = true;
+
+        VkMemoryRequirements memory_requirements;
+        vkGetBufferMemoryRequirements(device_.handle(), buffer->handle(), &memory_requirements);
+
+        return allocate_memory(std::move(memory_requirements), memory_property_types, linear_memory);
+    }
+
+    template<>
+    std::shared_ptr<resource::device_memory>
+    memory_manager::allocate_memory(std::shared_ptr<resource::image> image, graphics::MEMORY_PROPERTY_TYPE memory_property_types)
+    {
+        auto const linear_memory = image->tiling() == graphics::IMAGE_TILING::LINEAR;
+
+        VkMemoryRequirements memory_requirements;
+        vkGetImageMemoryRequirements(device_.handle(), image->handle(), &memory_requirements);
+
+        return allocate_memory(std::move(memory_requirements), memory_property_types, linear_memory);
+    }
+
+    std::shared_ptr<resource::device_memory>
+    memory_manager::allocate_memory(VkMemoryRequirements &&memory_requirements, graphics::MEMORY_PROPERTY_TYPE memory_property_types, bool linear)
+    {
+        return std::shared_ptr<resource::device_memory>();
+    }
+}
--- VulkanIsland/src/resources/memory_manager.hxx
+++ VulkanIsland/src/resources/memory_manager.hxx
@@ -13,6 +13,8 @@ namespace resource
 {
     class buffer;
     class image;
+
+    class memory_manager;
 }
 
 namespace resource
@@ -38,6 +40,8 @@ namespace resource
         VkMemoryPropertyFlags properties_;
 
         bool linear_;
+
+        friend resource::memory_manager;
     };
 
     class memory_manager final {
@@ -46,15 +50,18 @@ namespace resource
         memory_manager(vulkan::device const &device);
         ~memory_manager();
 
-        template<class T, typename std::enable_if_t<mpl::is_one_of_v<T, VkBuffer, VkImage>>* = nullptr>
-        std::shared_ptr<resource::device_memory> allocate_memory(T buffer, graphics::MEMORY_PROPERTY_TYPE);
+        template<class T, typename std::enable_if_t<mpl::is_one_of_v<T, std::shared_ptr<resource::buffer>, std::shared_ptr<resource::image>>>* = nullptr>
+        std::shared_ptr<resource::device_memory> allocate_memory(T buffer, graphics::MEMORY_PROPERTY_TYPE memory_property_types);
 
     private:
 
         static std::size_t constexpr kBLOCK_ALLOCATION_SIZE{0x800'0000};   // 128 MB
 
-        vulkan::device const &device;
+        vulkan::device const &device_;
 
         std::size_t total_allocated_size_{0}, buffer_image_granularity_{0};
+
+        std::shared_ptr<resource::device_memory>
+        allocate_memory(VkMemoryRequirements &&memory_requirements, graphics::MEMORY_PROPERTY_TYPE, bool linear);
     };
 }
