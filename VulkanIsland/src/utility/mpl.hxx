39c0d32bfa7096a208156b799611b2c9d056d9b6
Almaz Kinziabulatov <auqolaq@gmail.com>
2020-03-22 21:18:15 +0300

    New MPL helpers.

--- VulkanIsland/src/utility/mpl.hxx
+++ VulkanIsland/src/utility/mpl.hxx
@@ -161,6 +161,15 @@ namespace mpl
     template<class T>
     concept enumeration = std::is_enum_v<T>;
 
+    template<class T>
+    concept arithmetic = std::is_arithmetic_v<T>;
+
+    template<class T, class... Ts>
+    concept all_arithmetic = std::conjunction_v<mpl::arithmetic<Ts>...>;
+
+    template<class... Ts, std::size_t N>
+    concept is_length_equal_to = sizeof...(Ts) == N;
+
     template<class T, class... Ts>
     concept variant_alternative = is_variant_alternative_v<T, Ts...>;
 }
