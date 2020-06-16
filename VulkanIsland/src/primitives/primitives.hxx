07282b23309bdb84730eca880c4011cadd9be703
Almaz Kinziabulatov <auqolaq@gmail.com>
2020-06-16 21:52:04 +0300

    New procedural primitive.

--- VulkanIsland/src/primitives/primitives.hxx
+++ VulkanIsland/src/primitives/primitives.hxx
@@ -11,4 +11,9 @@ namespace primitives
     std::vector<std::byte>
     generate_plane(float width, float height, std::uint32_t hsegments, std::uint32_t vsegments,
                    graphics::vertex_layout const &vertex_layout, glm::vec4 const &color = glm::vec4{1});
+
+    std::vector<std::byte>
+    generate_box(float width, float height, float depth, std::uint32_t hsegments, std::uint32_t vsegments, std::uint32_t dsegments,
+                 graphics::vertex_layout const &vertex_layout, glm::vec4 const &color = glm::vec4{1});
 }
+
